8146cf89f6a4f27976defb3ce39e9b9314ee0f1b
Dr-N0 <beckettjenen@gmail.com>
2023-08-29 10:34:20 -0400

    Characters Page Formatting + Logo + Big Css P2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useEffect } from "react";
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
@@ -9,11 +12,40 @@ export const metadata: Metadata = {
   description: 'Armory for your DND needs',
 }
 
+function invertColor(hex: string): string {
+  if (hex.indexOf('#') === 0) {
+      hex = hex.slice(1);
+  }
+  // convert 3-digit hex to 6-digits.
+  if (hex.length === 3) {
+      hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+  }
+  // invert color components
+  const r = (255 - parseInt(hex.slice(0, 2), 16)).toString(16);
+  const g = (255 - parseInt(hex.slice(2, 4), 16)).toString(16);
+  const b = (255 - parseInt(hex.slice(4, 6), 16)).toString(16);
+  // pad each with zeros and return
+  return '#' + padZero(r) + padZero(g) + padZero(b);
+}
+
+function padZero(str: string, len: number = 2): string {
+  const zeros = new Array(len).join('0');
+  return (zeros + str).slice(-len);
+}
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+
+  useEffect(() => {
+    let randomColor: string = Math.floor(Math.random()*16777215).toString(16);
+    let oppositeRandomColor = invertColor(randomColor);
+    document.documentElement.style.setProperty('--random-color', `#${randomColor}`);
+    document.documentElement.style.setProperty('--opposite-random-color', `${oppositeRandomColor}`);
+  }, []);
+
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
