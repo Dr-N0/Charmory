82015c46c6c9f1c0ecb381168cf6474802a34e87
Dr-N0 <beckettjenen@gmail.com>
2023-08-29 10:34:53 -0400

    Characters Page Formatting + Logo + Big Css P7

---
+++ src/components/CharacterSearchBar.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+import style from '@/src/components/CharacterSearchBar.module.css'
+
+export default function CharacterSearchBar() {
+  return (
+    <div>
+        <input></input>
+    </div>
+  );
+}
---
+++ src/components/Nav.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import style from '@/src/app/Home.module.css'
+
+export default function Nav() {
+  return (
+    <nav className={style.navigation}>
+      <Link href={`/`}>
+          <h1>
+              <span className={style.ch}>Ch</span>
+              <span className={style.ar}>ar</span>
+              <span className={style.mory}>mory</span>
+          </h1>
+      </Link>
+    </nav>
+  );
+}
