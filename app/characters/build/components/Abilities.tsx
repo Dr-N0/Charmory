37b0ed4d2b0a9e12605a4d70b70897b87bf6290f
Dr-N0 <beckettjenen@gmail.com>
2023-09-01 13:51:37 -0400

    Lazy loading + Separated Components (css) p4

---
+++ app/characters/build/components/Abilities.tsx
@@ -0,0 +1,12 @@
+export default function Abilities() {
+
+    return (
+      <section id='abilities'>
+        <h1>
+          Abilities
+        </h1>
+        <p>askldfjaskldfjklsadfjklsafjlksajklfsdakljfkjlasd</p>
+      </section>
+    );
+  }
+  
---
+++ app/characters/build/components/Class.tsx
@@ -0,0 +1,11 @@
+export default function Class() {
+
+  return (
+    <section id='class'>
+      <h1>
+        Class
+      </h1>
+      <p>askldfjaskldfjklsadfjklsafjlksajklfsdakljfkjlasd</p>
+    </section>
+  );
+}
