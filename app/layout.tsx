9027247ba92319a84cb671076a0d324cf1648131
Dr-N0 <beckettjenen@gmail.com>
2023-09-03 19:32:31 -0400

    Hella css done! Changed Background to particles P1

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import ColorLoader from './components/ColorLoader';
 import Nav from '@/app/components/Nav'
+import FireParticles from './components/FireParticles';
 import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ['latin'] })
@@ -21,9 +22,19 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        
         <ColorLoader></ColorLoader>
+        <FireParticles />
         <Nav />
         <Providers>{children}</Providers>
+        <div className="page-bg"></div>
+        <div className="animation-wrapper">
+          <div className="particle particle-1"></div>
+          <div className="particle particle-2"></div>
+          <div className="particle particle-3"></div>
+          <div className="particle particle-4"></div>
+        </div>
+        <div className='page-wrapper'></div>
       </body>
     </html>
   )
