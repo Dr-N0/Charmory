968a44ccd9f5ff5515861d490cdb8c01486960c4
Dr-N0 <beckettjenen@gmail.com>
2023-09-05 15:45:16 -0400

    Touched up components + Connected BE data to FE P7

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -17,6 +17,42 @@ async function main() {
         create: [
           {
             name: 'Percival "Percy" Fredrickstein Von Musel Klossowski de Rolo III',
+            level: 5,
+            speed: 30,
+            armorClass: 14,
+            initiative: 4,
+            proficencies: {
+              create: {
+                savingStrength: 'PROFICENT',
+                savingDexterity: 'PROFICENT',
+                savingConstitution: 'NONE',
+                savingIntelligence: 'PROFICENT',
+                savingWisdom: 'NONE',
+                savingCharisma: 'PROFICENT',
+                
+                acrobatics: 'NONE',
+                animalHandling: 'PROFICENT',
+                arcana: 'NONE',
+                athletics: 'NONE',
+                deception: 'PROFICENT',
+                history: 'NONE',
+                insight: 'NONE',
+                intimidation: 'PROFICENT',
+                investigation: 'NONE',
+                medicine: 'NONE',
+                nature: 'NONE',
+                perception: 'PROFICENT',
+                performance: 'NONE',
+                persuasion: 'NONE',
+                religion: 'NONE',
+                sleightOfHand: 'PROFICENT',
+                stealth: 'NONE',
+                survival: 'PROFICENT',
+              }
+            },
+            inspiration: true,
+            healthMax: 29,
+            healthCurrent: 20,
             race: {
               create: {
                 name: 'Race Name'
@@ -51,6 +87,42 @@ async function main() {
           },
           {
             name: 'EXTRA Percival Fredrickstein Von Musel Klossowski de Rolo III',
+            level: 17,
+            speed: 40,
+            armorClass: 12,
+            initiative: 2,
+            proficencies: {
+              create: {
+                savingStrength: 'NONE',
+                savingDexterity: 'EXTRA',
+                savingConstitution: 'NONE',
+                savingIntelligence: 'NONE',
+                savingWisdom: 'NONE',
+                savingCharisma: 'NONE',
+                
+                acrobatics: 'NONE',
+                animalHandling: 'NONE',
+                arcana: 'NONE',
+                athletics: 'NONE',
+                deception: 'NONE',
+                history: 'NONE',
+                insight: 'NONE',
+                intimidation: 'NONE',
+                investigation: 'NONE',
+                medicine: 'EXTRA',
+                nature: 'NONE',
+                perception: 'NONE',
+                performance: 'NONE',
+                persuasion: 'NONE',
+                religion: 'EXTRA',
+                sleightOfHand: 'NONE',
+                stealth: 'NONE',
+                survival: 'EXTRA',
+              },
+            },
+            inspiration: false,
+            healthMax: 40,
+            healthCurrent: 10,
             race: {
               create: {
                 name: 'Extra Race Name'
