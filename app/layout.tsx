32be2e2068290b0ceaf06e522e0a94497a000816
Dr-N0 <beckettjenen@gmail.com>
2023-09-06 19:08:09 -0400

    Some animation changes p2

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,12 +24,24 @@ export default async function RootLayout({
         <ColorLoader></ColorLoader>
         <Nav />
         <Providers>{children}</Providers>
+        {/* USE FIRE/ASH ANIMATION FOR OPENING PAGE */}
+        {/* USE NETFLIX FOR CHARACTER SHEET AND CREATE */}
+        {/* Different particle background per race/class they choose */}
         <div className="page-bg"></div>
         <div className="animation-wrapper">
-          <div className="particle particle-1"></div>
+          <div className='light x1'></div>
+          <div className='light x2'></div>
+          <div className='light x3'></div>
+          <div className='light x4'></div>
+          <div className='light x5'></div>
+          <div className='light x6'></div>
+          <div className='light x7'></div>
+          <div className='light x8'></div>
+          <div className='light x9'></div>
+          {/* <div className="particle particle-1"></div>
           <div className="particle particle-2"></div>
           <div className="particle particle-3"></div>
-          <div className="particle particle-4"></div>
+          <div className="particle particle-4"></div> */}
         </div>
         <div className='page-wrapper'></div>
       </body>
