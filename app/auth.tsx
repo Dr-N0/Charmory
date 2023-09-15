2a35c8853687fad84108c475c57c1ff8e4027836
Dr-N0 <beckettjenen@gmail.com>
2023-09-15 15:40:50 -0400

    Big push! New Builder + Small styling changes P1

--- app/auth.tsx
+++ app/auth.tsx
@@ -4,9 +4,9 @@ import { signIn, signOut } from 'next-auth/react'
 import style from '@/app/Home.module.css'
 
 export const LoginButton = () => {
-  return <button className={`${style.hvrFade} ${style.solidBackground}`} onClick={() => signIn()}>Sign in</button>
+  return <button className={`${style.button} ${style.hvrFade} ${style.solidBackground}`} onClick={() => signIn()}>Sign in</button>
 }
 
 export const LogoutButton = () => {
-  return <button className={`${style.hvrFade} ${style.solidBackground}`} onClick={() => signOut()}>Sign Out</button>
+  return <button className={`${style.button} ${style.hvrFade} ${style.solidBackground}`} onClick={() => signOut()}>Sign Out</button>
 }
