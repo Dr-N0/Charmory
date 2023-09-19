63990aeaf2415eb6f192353ba92465138af53c86
Dr-N0 <beckettjenen@gmail.com>
2023-09-19 15:53:42 -0400

    Interactive comp skeleton + char sheet fixes p5

---
+++ app/characters/[id]/interactive_components/Background.tsx
@@ -0,0 +1,16 @@
+'use client'
+import { useState } from 'react'
+
+import style from './Interactive.module.css'
+
+export default function Background({
+    
+}: any) {
+
+
+    return (
+        <div className={style.contentContainer}>
+            Background
+        </div>
+    );
+}
---
+++ app/characters/[id]/interactive_components/Combat.tsx
@@ -0,0 +1,15 @@
+'use client'
+import { useState } from 'react'
+
+import style from './Interactive.module.css'
+
+export default function Combat({
+    
+}: any) {
+
+    return (
+        <div className={style.contentContainer}>
+            Combat
+        </div>
+    );
+}
