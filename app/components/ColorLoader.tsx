20d87aa2d88f0f9fc627cfa333d6de0a23afe9e6
Dr-N0 <beckettjenen@gmail.com>
2023-09-21 23:08:49 -0400

    New Db Schema and seeding + Architecture fix p5

--- app/components/ColorLoader.tsx
+++ app/components/ColorLoader.tsx
@@ -16,41 +16,40 @@ function invertColor(hex: string): string {
     const b = (255 - parseInt(hex.slice(4, 6), 16)).toString(16);
     // pad each with zeros and return
     return '#' + padZero(r) + padZero(g) + padZero(b);
-  }
+}
   
-  function padZero(str: string, len: number = 2): string {
+function padZero(str: string, len: number = 2): string {
     const zeros = new Array(len).join('0');
     return (zeros + str).slice(-len);
-  }
+}
 
 export default function ColorLoader() {
 
     useEffect(() => {
-      let randomColor: string = Math.random().toString(16).slice(2, 8);
-      let randomSection = Math.floor(Math.random() * 5) + 1;
-      let oppositeRandomColor = invertColor(randomColor);
-      switch (randomSection){
-        case 1:
-          randomColor = '00' + randomColor.slice(2);
-          randomColor = randomColor.slice(0, -2) + "ff";
-          break;
-        case 2:
-          randomColor = '00ff' + randomColor.slice(4);
-          break;
-        case 3:
-          randomColor = randomColor.slice(0, -4) + "00ff";
-          break;
-        case 4:
-          randomColor = "ff00" + randomColor.slice(4);
-          break;
-        case 5:
-          randomColor = 'ff' + randomColor.slice(2);
-          randomColor = randomColor.slice(0, -2) + "00";
-          break;
-      }
-      console.log(randomColor)
-      document.documentElement.style.setProperty('--random-color', `#${randomColor}`);
-      document.documentElement.style.setProperty('--opposite-random-color', `${oppositeRandomColor}`);
+        let randomColor: string = Math.random().toString(16).slice(2, 8);
+        let randomSection = Math.floor(Math.random() * 5) + 1;
+        let oppositeRandomColor = invertColor(randomColor);
+        switch (randomSection){
+            case 1:
+                randomColor = '00' + randomColor.slice(2);
+                randomColor = randomColor.slice(0, -2) + "ff";
+                break;
+            case 2:
+                randomColor = '00ff' + randomColor.slice(4);
+                break;
+            case 3:
+                randomColor = randomColor.slice(0, -4) + "00ff";
+                break;
+            case 4:
+                randomColor = "ff00" + randomColor.slice(4);
+                break;
+            case 5:
+                randomColor = 'ff' + randomColor.slice(2);
+                randomColor = randomColor.slice(0, -2) + "00";
+                break;
+        }
+        document.documentElement.style.setProperty('--random-color', `#${randomColor}`);
+        document.documentElement.style.setProperty('--opposite-random-color', `${oppositeRandomColor}`);
     }, []);
 
     return (
